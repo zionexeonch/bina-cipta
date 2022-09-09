b06c31af582cb6b9111911bda78c54642d0c0ccf
zionexeonch <zionexeon@gmail.com>
2022-09-09 20:11:29 +0700

    RED

---
+++ resources/views/Home/layouts/navbar1.blade.php
@@ -0,0 +1,13 @@
+<nav class="bg-white border-gray-200 dark:bg-gray-900">
+    <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl px-4 md:px-6 py-2.5">
+        <a href="https://flowbite.com" class="flex items-center">
+            <img src="https://flowbite.com/docs/images/logo.svg" class="mr-3 h-6 sm:h-9" alt="Flowbite Logo">
+            <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">BINA CIPTA</span>
+        </a>
+        <div class="flex items-center">
+            <a href="tel:5541251234"
+                class="mr-6 text-sm font-medium text-gray-500 dark:text-white hover:underline">(555) 412-1234</a>
+            <a href="#" class="text-sm font-medium text-blue-600 dark:text-blue-500 hover:underline">Login</a>
+        </div>
+    </div>
+</nav>
---
+++ resources/views/Home/layouts/navbar2.blade.php
@@ -0,0 +1,24 @@
+<nav class="bg-gray-50 dark:bg-gray-700">
+    <div class="py-3 px-4 mx-auto max-w-screen-xl md:px-6">
+        <div class="flex text-center items-center">
+            <ul class="flex flex-row mt-0 mx-auto space-x-8 text-base font-bold">
+                <li>
+                    <a href="/" class="text-gray-900 dark:text-white dark:hover:text-red-500 hover:text-red-600"
+                        aria-current="page">Home</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="text-gray-900 dark:text-white dark:hover:text-red-500 hover:text-red-600">Company</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="text-gray-900 dark:text-white dark:hover:text-red-500 hover:text-red-600">Team</a>
+                </li>
+                <li>
+                    <a href="#"
+                        class="text-gray-900 dark:text-white dark:hover:text-red-500 hover:text-red-600">Features</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
